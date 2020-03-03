9c1e62a489811080ca74d7336ea81c208c95da54
RoyDg <rdg01834@gmail.com>
2020-03-04 02:12:53 +0600

    null vs undefined done till now

---
+++ undefinedVsNull.js
@@ -0,0 +1,23 @@
+// no valu
+let name;
+console.log(name);
+
+// not return
+function add(num1, num2){
+    console.log(num1+num2);
+}
+const result = add (13, 14);
+console.log(result);
+
+//parameter value not passed
+function adding(num1, num2) {
+    console.log(num1 + num2);
+    return result1;
+}
+const result1 = adding (20);
+console.log(result1);
+
+/*null means empty. Search Results
+Featured snippet from the web
+undefined means a variable has been declared but has not yet been assigned a value.
+On the other hand, null is an assignment value. It can be assigned to a variable as a representation of no value. Also, undefined and null are two distinct types: undefined is a type itself (undefined) while null is an object. */
