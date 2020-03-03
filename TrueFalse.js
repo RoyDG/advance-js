10e80bba65c419f9b7387727c7346a07bd914ee5
RoyDg <rdg01834@gmail.com>
2020-03-04 01:44:40 +0600

    tru false done till now

---
+++ TrueFalse.js
@@ -0,0 +1,22 @@
+//if age value = 0 it return false
+//other number it will return true
+const age = -4;
+if(age){
+    console.log("true")
+}
+else{
+    console.log("not true")
+}
+
+//in empty string it will return false
+const name = "";
+if (name) {
+    console.log("true")
+}
+else {
+    console.log("not true")
+}
+
+// 0 , empty string "", NaN, null,
+// undefined, false | those always returns false.
+// apart from them everything are true. 
